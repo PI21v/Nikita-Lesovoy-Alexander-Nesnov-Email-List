c6e2a91d8b2a15d01fb42b8fd2cb3a9ce30c739e
anesnov <115095350+anesnov@users.noreply.github.com>
2023-05-30 14:30:10 +0300

    Add files via upload

---
+++ EmailList/TextParse.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace EmailList
+{
+    partial class TextParse
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            richTextBox1 = new RichTextBox();
+            button1 = new Button();
+            button2 = new Button();
+            dataGridView1 = new DataGridView();
+            button3 = new Button();
+            materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(28, 30);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(492, 346);
+            richTextBox1.TabIndex = 0;
+            richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(28, 383);
+            button1.Name = "button1";
+            button1.Size = new Size(129, 55);
+            button1.TabIndex = 1;
+            button1.Text = "Загрузить текст";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(163, 383);
+            button2.Name = "button2";
+            button2.Size = new Size(129, 55);
+            button2.TabIndex = 2;
+            button2.Text = "Найти Email";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(526, 30);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(262, 346);
+            dataGridView1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(526, 383);
+            button3.Name = "button3";
+            button3.Size = new Size(129, 55);
+            button3.TabIndex = 4;
+            button3.Text = "Добавить выбранное в список";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // materialLabel1
+            // 
+            materialLabel1.AutoSize = true;
+            materialLabel1.Depth = 0;
+            materialLabel1.Font = new Font("Roboto", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            materialLabel1.ForeColor = Color.FromArgb(222, 0, 0, 0);
+            materialLabel1.Location = new Point(28, 8);
+            materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            materialLabel1.Name = "materialLabel1";
+            materialLabel1.Size = new Size(214, 19);
+            materialLabel1.TabIndex = 5;
+            materialLabel1.Text = "Кол-во Найденных Адресов:";
+            // 
+            // materialLabel2
+            // 
+            materialLabel2.AutoSize = true;
+            materialLabel2.Depth = 0;
+            materialLabel2.Font = new Font("Roboto", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            materialLabel2.ForeColor = Color.FromArgb(222, 0, 0, 0);
+            materialLabel2.Location = new Point(248, 8);
+            materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
+            materialLabel2.Name = "materialLabel2";
+            materialLabel2.Size = new Size(17, 19);
+            materialLabel2.TabIndex = 6;
+            materialLabel2.Text = "0";
+            // 
+            // TextParse
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(materialLabel2);
+            Controls.Add(materialLabel1);
+            Controls.Add(button3);
+            Controls.Add(dataGridView1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(richTextBox1);
+            Name = "TextParse";
+            Text = "TextParse";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private RichTextBox richTextBox1;
+        private Button button1;
+        private Button button2;
+        private DataGridView dataGridView1;
+        private Button button3;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel2;
+    }
+}
---
+++ EmailList/TextParse.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Threading;
+
+namespace EmailList
+{
+    public partial class TextParse : Form
+    {
+        public TextParse()
+        {
+            InitializeComponent();
+            // Создаем столбцы DataGridView
+            DataGridViewTextBoxColumn emailColumn = new DataGridViewTextBoxColumn();
+            emailColumn.HeaderText = "Email";
+            emailColumn.Name = "EmailColumn";
+
+            DataGridViewTextBoxColumn descriptionColumn = new DataGridViewTextBoxColumn();
+            descriptionColumn.HeaderText = "Описание";
+            descriptionColumn.Name = "DescriptionColumn";
+
+            // Добавляем столбцы к DataGridView
+            dataGridView1.Columns.Add(emailColumn);
+            dataGridView1.Columns.Add(descriptionColumn);
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Создаем диалоговое окно открытия файла
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Открываем выбранный файл и считываем его содержимое
+                string fileName = openFileDialog.FileName;
+                string fileContent = File.ReadAllText(fileName);
+
+                // Отображаем содержимое файла в RichTextBox
+                richTextBox1.Text = fileContent;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            materialLabel2.Text = "0";
+            int sc = 0;
+            // Получаем текст из RichTextBox
+            string text = richTextBox1.Text;
+
+            // Используем регулярное выражение для поиска email адресов
+            string pattern = @"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b";
+            MatchCollection matches = Regex.Matches(text, pattern);
+            dataGridView1.Rows.Clear();
+            // Подсвечиваем найденные email адреса цветом
+            foreach (Match match in matches)
+            {
+                int startIndex = match.Index;
+                int length = match.Length;
+                richTextBox1.Select(startIndex, length);
+                richTextBox1.SelectionBackColor = System.Drawing.Color.Yellow;
+                richTextBox1.SelectionColor = System.Drawing.Color.Black;
+                string email = match.Value;
+                dataGridView1.Rows.Add(email, "");
+                sc++;
+            }
+            materialLabel2.Text = sc.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            {
+                // Проверяем, что текущая строка не является последней пустой строкой
+                if (!row.IsNewRow)
+                {
+                    string email = row.Cells[dataGridView1.Columns["EmailColumn"].Index].Value.ToString();
+                    string opis = row.Cells[dataGridView1.Columns["DescriptionColumn"].Index].Value.ToString();
+                    bool isValidEmail = Regex.IsMatch(email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$");
+
+                    if (isValidEmail)
+                    {
+                        try
+                        {
+                            string exePath = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+                            string filePath = Path.Combine(exePath, "tmp.txt");
+                            string data = $"{email};{opis}";
+
+                            File.WriteAllText(filePath, data);
+
+                            MessageBox.Show("Данные успешно записаны в файл.", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Произошла ошибка при записи данных в файл: " + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Неверный адрес почты", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+
+
+        }
+    }
+}
